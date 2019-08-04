9876e91adb2b80c5d50cfac4158df9b68cb7579e
xiuda3411 <1157867526@qq.com>
2019-08-05 00:26:24 +0800

    项目管理模块

---
+++ Excellent-Room-sl-dev/src/main/java/cn/edu/cqupt/mislab/excellentroom/exception/MyException.java
@@ -0,0 +1,10 @@
+package cn.edu.cqupt.mislab.excellentroom.exception;
+
+/**
+ * @program: Excellent-Room-sl-dev
+ * @description: 自定义异常类
+ * @author: 宋丽
+ * @create: 2019-08-05 00:25
+ **/
+public class MyException {
+}
